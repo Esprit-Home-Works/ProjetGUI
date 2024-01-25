f1d7a4a045216555e6ae4045134d8de959dce891
RaedCHARRAD <raed.charrad91@gmail.com>
2024-01-25 22:49:10 +0100

    add database Connection

---
+++ GUIProject/src/main/java/com/project/guiproject/test/Main.java
@@ -0,0 +1,10 @@
+package com.project.guiproject.test;
+
+import com.project.guiproject.utils.MyDataBase;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        MyDataBase myDataBase = new MyDataBase();
+    }
+}
---
+++ GUIProject/src/main/java/com/project/guiproject/utils/MyDataBase.java
@@ -0,0 +1,36 @@
+package com.project.guiproject.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyDataBase {
+    private static MyDataBase instance;
+    private static final String URL = "jdbc:mysql://localhost:3306/projetgui";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private Connection connection;
+
+    public MyDataBase() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection established");
+        } catch (Exception e) {
+            System.out.println("Connection refused");
+
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static MyDataBase getInstace() {
+        if (instance == null) {
+            instance = new MyDataBase();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
