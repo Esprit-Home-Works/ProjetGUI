d2b9476e87b0cafd0b64792ab38d7249ee92a3b1
RaedCHARRAD <raed.charrad91@gmail.com>
2024-01-26 10:27:53 +0100

    add crud and authentication methods

---
+++ GUIProject/src/main/java/com/project/guiproject/services/UserService.java
@@ -0,0 +1,105 @@
+package com.project.guiproject.services;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.project.guiproject.models.User;
+import com.project.guiproject.utils.MyDataBase;
+
+public class UserService implements IService<User> {
+
+    private Connection connection;
+
+    public UserService() {
+        connection = MyDataBase.getInstace().getConnection();
+    }
+
+    @Override
+    public void add(User User) throws SQLException {
+        String req = "INSERT INTO user (id,username,password,email) VALUES ('"
+                + User.getId() + "', '" + User.getUsername() + "', '" + User.getPassword() + "', '"
+                + User.getEmail() + "')";
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(req);
+    }
+
+    @Override
+    public void update(User User) throws SQLException {
+        String req = "UPDATE user SET id = ?,username = ?, password = ? ,email = ? WHERE id = ?";
+        PreparedStatement PS = connection.prepareStatement(req);
+        PS.setInt(1, User.getId());
+        PS.setString(2, User.getUsername());
+        PS.setString(3, User.getPassword());
+        PS.setString(4, User.getEmail());
+        PS.setInt(5, User.getId());
+        PS.executeUpdate();
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        String req = "DELETE FROM user WHERE id = ?";
+        PreparedStatement PS = connection.prepareStatement(req);
+        try {
+            PS.setInt(1, id);
+            PS.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public User getById(int id) throws SQLException {
+        String query = "SELECT * FROM user WHERE id = ?";
+        PreparedStatement PS = connection.prepareStatement(query);
+        PS.setInt(1, id);
+        PS.executeQuery();
+        ResultSet RS = PS.getResultSet();
+        return RS.next()
+                ? new User(RS.getInt("id"), RS.getString("username"), RS.getString("password"), RS.getString("email"))
+                : null;
+
+    }
+
+    @Override
+    public List<User> get() throws SQLException {
+        List<User> User = new ArrayList<>();
+        String query = "SELECT * FROM user";
+        PreparedStatement PS = connection.prepareStatement(query);
+        PS.executeQuery();
+        ResultSet RS = PS.getResultSet();
+        while (RS.next()) {
+            User.add(new User(RS.getInt("id"), RS.getString("username"), RS.getString("password"),
+                    RS.getString("email")));
+        }
+        return User;
+    }
+
+    // Login and Signup Methods
+    public boolean login(String username, String password) {
+        String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
+        try {
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public void signup(String username, String password, String email) throws SQLException {
+        User user = new User(username, password, email);
+        add(user);
+    }
+
+}
--- GUIProject/src/main/java/com/project/guiproject/test/Main.java
+++ GUIProject/src/main/java/com/project/guiproject/test/Main.java
@@ -3,17 +3,20 @@ package com.project.guiproject.test;
 import java.sql.Date;
 
 import com.project.guiproject.models.Match;
+import com.project.guiproject.models.User;
 import com.project.guiproject.services.MatchService;
+import com.project.guiproject.services.UserService;
 import com.project.guiproject.utils.MyDataBase;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         MyDataBase myDataBase = new MyDataBase();
-        MatchService matchService = new MatchService();
+        // MatchService matchService = new MatchService();
+        UserService user = new UserService();
         try {
-            // matchService.add(new Match(180, "match 1", "test Match",
-            // "MT_001", new Date(2021, 1, 1),
+            user.add(new User(1, "kamel", "12345",
+                    "kamel.htira@gmail.com"));
             // new Date(2021, 1, 1)));
             // matchService.add(new Match(180, "match 2", "test Match",
             // "MT_001", new Date(2021, 1, 1),
@@ -22,14 +25,14 @@ public class Main {
             // "MT_001", new Date(2021, 1, 1),
             // new Date(2021, 1, 1)));
             // matchService.delete(1);
-            Match mt = matchService.getById(2);
-            System.out.println(mt);
+            // Match mt = matchService.getById(2);
+            // System.out.println(mt);
 
-            System.out.println("the list of matches: ");
+            // System.out.println("the list of matches: ");
 
-            matchService.get().forEach(match -> {
-                System.out.println(match);
-            });
+            // matchService.get().forEach(match -> {
+            // System.out.println(match);
+            // });
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
