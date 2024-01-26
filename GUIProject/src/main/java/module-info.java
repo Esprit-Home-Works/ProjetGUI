5140f3a9f4ddd4bb6836a9cf67f8952de43a825a
Yassine SBOUI <sboui@neoledge.com>
2024-01-26 11:07:18 +0100

    Tournament

--- GUIProject/src/main/java/module-info.java
+++ GUIProject/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module com.project.guiproject {
     requires javafx.controls;
     requires javafx.fxml;
-    requires  java.sql;
+    requires java.sql;
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.project.guiproject to javafx.fxml;
     exports com.project.guiproject.test;
     opens com.project.guiproject.test to javafx.fxml;
+    exports com.project.guiproject.models;
+    opens com.project.guiproject.models to javafx.fxml;
 }
